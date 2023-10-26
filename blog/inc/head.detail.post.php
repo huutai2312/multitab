6ed34cf95f0577de76e58b7e3111ff173b750184
NHT <71449181+huutai2312@users.noreply.github.com>
2023-10-26 09:20:00 +0700

    Update head.detail.post.php

--- blog/inc/head.detail.post.php
+++ blog/inc/head.detail.post.php
@@ -1,10 +1,9 @@
 <?php
 include_once __DIR__ . "/../classes/class.brain.php";
-
 ?>
 <!DOCTYPE html>
 <html lang="vi">
-
+ 
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
