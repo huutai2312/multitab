12b5a56f9a2e2e7fcff516e4eaff90e00de2c1cd
NHT <71449181+huutai2312@users.noreply.github.com>
2023-10-26 09:19:42 +0700

    Update footer.home.php

--- blog/inc/footer.home.php
+++ blog/inc/footer.home.php
@@ -5,7 +5,7 @@
           <div class="container">
             <div class="footer_btm_in">
               <div class="footer_copyright">
-                <p>© 2023 <a href="#">NHT</a></p>
+                <p>© 2023 <a href="">NHT</a></p>
               </div>
             </div>
           </div>
@@ -17,7 +17,7 @@
 <div class="frenify-cursor cursor-outer" data-default="yes" data-link="yes" data-slider="yes"></div>
 <div class="frenify-cursor cursor-inner" data-default="yes" data-link="yes" data-slider="yes"></div><a class="spide_fn_totop" href="#" aria-label="Go to top"><img class="fn__svg" src="https://frenify.com/work/envato/frenify/wp/spide/1/wp-content/themes/spide/framework/svg/arrow2.svg" alt="svg"></a>
 </div><!-- HTML ends here -->
-<div class="clearfix"></div>
+<div class="clearfix"></div> 
 <script type="text/javascript" src="public/assets/js/index.js" id="swv-js"></script>
 <script type="text/javascript" src="public/assets/js/index2.js" id="contact-form-7-js"></script>
 <script type="text/javascript" src="public/assets/js/magnific.popup.js" id="magnific.popup-js"></script>
